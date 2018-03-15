474bd56948d085b964091ae9a33720c1d5c334d3
ArthurBocquiny <abocquiny@gmail.com>
2018-03-15 11:41:50 +0100

    modif profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -14,6 +14,7 @@ class ProfilController extends Controller
     public function index()
     {
         $user = $this->getUser();
+        dump($user);
         $userCountry = $this->getUser()->getCountry();
         $country = Intl::getRegionBundle()->getCountryNames();
         foreach($country as $key => $value)
